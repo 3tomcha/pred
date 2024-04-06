873957154fe83fa2d6e82c2ad3684001ffd57768
Tomoya Kobayashi <tomocha.marika@gmail.com>
2024-04-06 10:40:56 +0900

    add header

--- pred.py
+++ pred.py
@@ -1,3 +1,4 @@
+import csv
 import ccxt
 import os
 from dotenv import load_dotenv
@@ -11,3 +12,12 @@ by = ccxt.bybit({
     'apiKey': api_key,
     'secret': secret,
 })
+
+# データ定義
+data = ["up_slope",  "under_slope", "sum_slope"]
+arr = []
+
+with open("pred.csv", "w") as f:
+  writer = csv.writer(f)
+  writer.writerow([data[0], data[1], data[2]])
+
